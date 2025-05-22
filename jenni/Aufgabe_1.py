b53045517424dadcf0af0e1db1090c3a0b67833f
JCerkaski <50489742+JCerkaski@users.noreply.github.com>
2025-05-22 17:43:26 +0200

    Aufgabe 1 + Rechner

--- jenni/Aufgabe_1.py
+++ jenni/Aufgabe_1.py
@@ -5,4 +5,19 @@ Created on Thu May 22 16:01:15 2025
 @author: User
 """
 
-test
+
+#name = input("Bitte gib deinen Namen ein")
+#name2 = "...Jenni Cerkaksi..."
+
+#cleaned_name = name2.strip(" .")
+
+#cleaned_name = cleaned_name.lower()
+
+#cleaned_name = cleaned_name.replace("Cerkaski", "test").title()
+
+#print(f'Hallo, {cleaned_name}')
+
+name = input("wie ist dein Name?")
+
+print(f'Moin, {name}!')
+
---
+++ jenni/Rechner.py
@@ -0,0 +1,34 @@
+# -*- coding: utf-8 -*-
+"""
+Created on Thu May 22 17:10:36 2025
+
+@author: User
+"""
+
+#zahl = int(input("Gib eine zahl ein: "))
+
+#Zahl1 = "5"
+#Zahl2 = "6"
+
+#Zahl1 = int(Zahl1)
+#Zahl2 = int(Zahl2)
+#Zahl = int(input("eingeben: "))
+
+#Zahl = 2.3
+
+#ergebnis = Zahl1 + Zahl2
+#print(ergebnis)
+
+Zahl1 = float(input("Erste zahl: "))
+
+Zahl2 = float(input("Zweite zahl: "))
+
+Ergebnis = Zahl1 / Zahl2
+
+Ergebnis2 = round(Zahl1 / Zahl2)
+
+Ergebnis = float(round(Ergebnis))
+
+print(f'das komplette Ergebnis ist, {Ergebnis}')
+print(f'das gerundete Ergebnis ist, {Ergebnis2}')
+
